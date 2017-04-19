0c2e99cc008173d888434bd3a5615e45de194176
Stephen Kelly <stevekm@users.noreply.github.com>
2017-04-19 13:15:30 -0400

    add message file to script

--- mutt.py
+++ mutt.py
@@ -45,10 +45,22 @@ def make_attachement_string(attachment_files):
     attachment_string = ''.join(attachment_strings)
     return(attachment_string)
 
-def mutt_mail(recipient_list, reply_to, subject_line, message, attachment_files):
+def get_file_contents(file):
+    '''
+    Return a string containing all lines in the file
+    '''
+    lines_list = []
+    with open(file) as f:
+        for line in f:
+            lines_list.append(line)
+    return(''.join(lines_list))
+
+def mutt_mail(recipient_list, reply_to, subject_line, message, message_file, attachment_files):
     '''
     Send the message with mutt
     '''
+    if message_file != None:
+        message = get_file_contents(message_file)
     attachment_string = make_attachement_string(attachment_files)
     command = '''
 export EMAIL="{0}"
@@ -75,6 +87,7 @@ parser.add_argument("attachment_files", type = str,  nargs='*', help="Files to b
 parser.add_argument("-s", default = '[mutt.py]', type = str, dest = 'subject_line', metavar = 'subject_line', help="Subject line for the email")
 parser.add_argument("-m", default = '~ This message was sent by the mutt.py email script ~', type = str, dest = 'message', metavar = 'message', help="Message for the body of the email")
 parser.add_argument("-rt", default = '', type = str, dest = 'reply_to', metavar = 'message', help="Message for the body of the email")
+parser.add_argument("-mf", default = None, type = str, dest = 'message_file', metavar = 'message file', help="File containing text to be included in the message body of the email")
 
 args = parser.parse_args()
 
@@ -82,7 +95,8 @@ recipient_list = args.recipient_list
 attachment_files = args.attachment_files
 subject_line = args.subject_line
 message = args.message
+message_file = args.message_file
 reply_to = args.reply_to
 
 if __name__ == "__main__":
-    mutt_mail(recipient_list = recipient_list, reply_to = reply_to, subject_line = subject_line, message = message, attachment_files = attachment_files)
+    mutt_mail(recipient_list = recipient_list, reply_to = reply_to, subject_line = subject_line, message = message, message_file = message_file, attachment_files = attachment_files)
